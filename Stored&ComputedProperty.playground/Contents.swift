334e0a38e8394f470860bffc738f5b1728790fd5
SIVA3161 <sivaguru3161@gmail.com>
2021-06-30 17:23:51 +0530

    Height calculation done using get set method

--- Stored&ComputedProperty.playground/Contents.swift
+++ Stored&ComputedProperty.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
+struct Person {
+    var name = ""
+    var heightInFeet = 0.0
+    var heightInCM:Double {
+        get {
+            return 30.48 * heightInFeet
+        }
+        set (newHeightInCM) {
+            heightInFeet = newHeightInCM / 30.48
+        }
+    }
+}
 
+
+var Siva = Person(name: "Siva", heightInFeet: 6.0)
+
+
+Siva.heightInFeet  // Shows height of Siva in Feet
+Siva.heightInCM    // Shows Siva's height in terms of CM.This is from get method
+
+
+//Declaring New heightInCM for Siva
+Siva.heightInCM = 140
+Siva.heightInFeet
